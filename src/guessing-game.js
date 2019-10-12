c4d1f7dbc1ec7274c7ae96eb3b548a25bb8e3e4c
qairkhan <shyngys94@gmai.com>
2019-10-12 18:59:10 +0300

    feat: add guess & lower

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,17 +2,18 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-        this.min=min
-        this.max=max
+        this.min=min;
+        this.max=max;
 
     }
 
     guess() {
-
+        this.average = Math.round((this.min + this.max) / 2);
+        return this.average;
     }
 
     lower() {
-
+        this.max = this.average;
     }
 
     greater() {
